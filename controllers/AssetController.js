842d8631afe3a1f927b5617251c98f13707ccc13
Goldweber <212570177@logon.ds.ge.com>
2016-07-25 15:33:08 -0400

    cleaned up

--- controllers/AssetController.js
+++ controllers/AssetController.js
@@ -1,7 +1,12 @@
 var WebSocket = require('ws');
+var request = require('request');
+var uuid = require('node-uuid');
 
 var UAAController = require('../controllers/UAAController');
 var uaaController = new UAAController();
+
+var UAAControllerTS = require('../controllers/UAAControllerTS');
+var uaaControllerTS = new UAAControllerTS();
 
 //var PayloadValidator = require('../controllers/PayloadValidator');
 
@@ -9,117 +14,176 @@ var config = require('../config/config.js');
 
 function AssetController() {
   this.headers = {};
-
+  this.model = null;
+  this.check = null;
   this.date = new Date();
+  this.access_token = null;
 
   // Fetch a token and set headers
   var self = this;
   uaaController.fetchToken(function(){
     self.setHeaders();
   });
+  uaaControllerTS.fetchTokenTS(function(){
+    console.log("UAA " + uaaControllerTS.getTokenTS());
+  });
 }
-
-// AssetController.prototype.setHeaders = function(){
-//   this.headers = {
-//     //'Authorization': 'Bearer ' + uaaController.getToken(),
-//     'Authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJjNzk0ODNmNS00MjA2LTQyYzQtYmI3Mi1iZWE5YmRjZWFmMmQiLCJzdWIiOiJhcHAtY2xpZW50LWlkIiwic2NvcGUiOlsidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQuaW5nZXN0IiwidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQucXVlcnkiLCJ1YWEucmVzb3VyY2UiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZC51c2VyIiwib3BlbmlkIiwidWFhLm5vbmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuMTBhMjFjOTQtMWM0NC00NGRjLTg2YTMtOWExYzdkNTA1NzZlLnVzZXIiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4LnVzZXIiXSwiY2xpZW50X2lkIjoiYXBwLWNsaWVudC1pZCIsImNpZCI6ImFwcC1jbGllbnQtaWQiLCJhenAiOiJhcHAtY2xpZW50LWlkIiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInJldl9zaWciOiIzYzA2Zjk2IiwiaWF0IjoxNDY3OTAyNTU5LCJleHAiOjE0Njc5NDU3NTksImlzcyI6Imh0dHBzOi8vNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhLnByZWRpeC11YWEucnVuLmF3cy11c3cwMi1wci5pY2UucHJlZGl4LmlvL29hdXRoL3Rva2VuIiwiemlkIjoiNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhIiwiYXVkIjpbImFwcC1jbGllbnQtaWQiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZCIsInVhYSIsIm9wZW5pZCIsInByZWRpeC1hc3NldC56b25lcy4xMGEyMWM5NC0xYzQ0LTQ0ZGMtODZhMy05YTFjN2Q1MDU3NmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4Il19.e-oRnAr3jrEojtCyPv-mI0cC4mQ-vtb92cQfDwe6Qd7vQtOdnQTQ269PShFYCaYw6q3Inoo3Madr_kZ_v0KdjpHXMvOLCr7aW6SxDlCFrBEARuExsz4Bk9OzDl96DJ4VByvK4JNiEbddYfkWPHb862OvmQf4bjYpi457LEivn4HxWm4x9-jjr9HWGVMPQpTBYQLujUuqs8NBK1o2r_etmS7Lo2PXhudLp7j0AvGIOp1S_7zAIJr5pe0xUvx_Ir1mOL13Dhi9NAe9xTv6ebety2H4NjVc3AQqm4fZQBD78GQbmC9T7Z8HJdkJs4LHatUkqEzSiHQjEbvPdm--pZTbKQ',
-//     'Predix-Zone-Id': config.timeseries.zoneId,
-//     'Content-Type': 'application/json'
-//    // 'Origin': 'https://' + config.application_uri
-//   };
-// };
-
-// AssetController.prototype.setHeaders = function(){
-//   this.headers = {
-//     //'Authorization': 'Bearer ' + uaaController.getToken(),
-//     'Authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJjNzk0ODNmNS00MjA2LTQyYzQtYmI3Mi1iZWE5YmRjZWFmMmQiLCJzdWIiOiJhcHAtY2xpZW50LWlkIiwic2NvcGUiOlsidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQuaW5nZXN0IiwidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQucXVlcnkiLCJ1YWEucmVzb3VyY2UiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZC51c2VyIiwib3BlbmlkIiwidWFhLm5vbmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuMTBhMjFjOTQtMWM0NC00NGRjLTg2YTMtOWExYzdkNTA1NzZlLnVzZXIiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4LnVzZXIiXSwiY2xpZW50X2lkIjoiYXBwLWNsaWVudC1pZCIsImNpZCI6ImFwcC1jbGllbnQtaWQiLCJhenAiOiJhcHAtY2xpZW50LWlkIiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInJldl9zaWciOiIzYzA2Zjk2IiwiaWF0IjoxNDY3OTAyNTU5LCJleHAiOjE0Njc5NDU3NTksImlzcyI6Imh0dHBzOi8vNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhLnByZWRpeC11YWEucnVuLmF3cy11c3cwMi1wci5pY2UucHJlZGl4LmlvL29hdXRoL3Rva2VuIiwiemlkIjoiNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhIiwiYXVkIjpbImFwcC1jbGllbnQtaWQiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZCIsInVhYSIsIm9wZW5pZCIsInByZWRpeC1hc3NldC56b25lcy4xMGEyMWM5NC0xYzQ0LTQ0ZGMtODZhMy05YTFjN2Q1MDU3NmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4Il19.e-oRnAr3jrEojtCyPv-mI0cC4mQ-vtb92cQfDwe6Qd7vQtOdnQTQ269PShFYCaYw6q3Inoo3Madr_kZ_v0KdjpHXMvOLCr7aW6SxDlCFrBEARuExsz4Bk9OzDl96DJ4VByvK4JNiEbddYfkWPHb862OvmQf4bjYpi457LEivn4HxWm4x9-jjr9HWGVMPQpTBYQLujUuqs8NBK1o2r_etmS7Lo2PXhudLp7j0AvGIOp1S_7zAIJr5pe0xUvx_Ir1mOL13Dhi9NAe9xTv6ebety2H4NjVc3AQqm4fZQBD78GQbmC9T7Z8HJdkJs4LHatUkqEzSiHQjEbvPdm--pZTbKQ',
-//     'Predix-Zone-Id': '10a21c94-1c44-44dc-86a3-9a1c7d50576e',
-//     'Content-Type' : 'application/json'
-//   };
-// };
 
 AssetController.prototype.setHeaders = function(){
   this.headers = {
     'Authorization': 'Bearer ' + uaaController.getToken(),
-    'Predix-Zone-Id': config.timeseries.zoneId,
-    'Origin': 'https://' + config.application_uri
+    'Predix-Zone-Id': config.asset.zoneId,
+    'Content-Type' : 'application/json'
   };
 };
 
-AssetController.prototype.getAssets = function(type, callback) {
-  request({
-    uri: config.asset.ingestUri + '/' + type, //URL to hit
-    method: 'GET',
-    headers: {
-        'Authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI4ZTk4MGFmOC01Yjg1LTRhMGYtOTQwNC01ZTIwNGMxYzIzMGIiLCJzdWIiOiJhcHAtY2xpZW50LWlkIiwic2NvcGUiOlsidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQuaW5nZXN0IiwidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQucXVlcnkiLCJ1YWEucmVzb3VyY2UiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZC51c2VyIiwib3BlbmlkIiwidWFhLm5vbmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuMTBhMjFjOTQtMWM0NC00NGRjLTg2YTMtOWExYzdkNTA1NzZlLnVzZXIiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4LnVzZXIiXSwiY2xpZW50X2lkIjoiYXBwLWNsaWVudC1pZCIsImNpZCI6ImFwcC1jbGllbnQtaWQiLCJhenAiOiJhcHAtY2xpZW50LWlkIiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInJldl9zaWciOiIzYzA2Zjk2IiwiaWF0IjoxNDY3OTgxMjIwLCJleHAiOjE0NjgwMjQ0MjAsImlzcyI6Imh0dHBzOi8vNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhLnByZWRpeC11YWEucnVuLmF3cy11c3cwMi1wci5pY2UucHJlZGl4LmlvL29hdXRoL3Rva2VuIiwiemlkIjoiNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhIiwiYXVkIjpbImFwcC1jbGllbnQtaWQiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZCIsInVhYSIsIm9wZW5pZCIsInByZWRpeC1hc3NldC56b25lcy4xMGEyMWM5NC0xYzQ0LTQ0ZGMtODZhMy05YTFjN2Q1MDU3NmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4Il19.hBCJvs38h0eBtpu5e9wWOL1f4nZn73knwt-YpaXGMGNfCla8Phu6YJ-d_g0oYTW-fFMwDhi-BVkVhKyEcclbzIi1AU-sqlnu9lBfLQDK5lCgjubZ15cqU2rKpIGGm8aeN3Xk4lRpw_167e6v4DTsoYIZ8K81FdSuPPA3_36PLXKZrcb6BCv9pxlzS4_KeFFt0UL6uWakxUq_qez5kHwgddkzXyzwDZe2pP2PgMsGR55oCxu8oiZKDfT5ZLNFNavmruYC3fr8Tm9O4PEG1ecel9SIt4DUA4SHvfmw1HHZNLTtony2_-BBPmOHNJPUk3fP9x2M-D6NWRa6zbkfNkCqVA',
-        'Predix-Zone-Id': config.asset.zoneId,
-        'Content-Type' : 'application/json'
-    },
-    //body: JSON.stringify(body)
-  }, function(error, response){
-    if(error) {
-        console.log(error);
-        callback('500', error);
-    } else {
-        console.log(response.statusCode);
-        callback(response.statusCode);
+AssetController.prototype.getAssets = function(req, callback) {
+  console.log("[INFO] Fetching asset...(B TOKEN = ) ");
+  var self = this;
+
+  var uuid1 = uuid.v1();
+console.log("uuid = " + uuid1);    // -> '02a2ce90-1432-11e1-8558-0b488e4fc115')
+  var options = {
+  url: config.asset.ingestUri+'/'+ req.params.type + '/' + req.params.model,
+  //method: 'GET',
+  headers: {
+    //'Authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI3MGM0NjA3MS05MTk3LTQ2MDYtOTBmYi02YTJiNGVkM2QwNjEiLCJzdWIiOiJhcHAtY2xpZW50LWlkIiwic2NvcGUiOlsidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQuaW5nZXN0IiwidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQucXVlcnkiLCJ1YWEucmVzb3VyY2UiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZC51c2VyIiwib3BlbmlkIiwidWFhLm5vbmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuMTBhMjFjOTQtMWM0NC00NGRjLTg2YTMtOWExYzdkNTA1NzZlLnVzZXIiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4LnVzZXIiXSwiY2xpZW50X2lkIjoiYXBwLWNsaWVudC1pZCIsImNpZCI6ImFwcC1jbGllbnQtaWQiLCJhenAiOiJhcHAtY2xpZW50LWlkIiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInJldl9zaWciOiIzYzA2Zjk2IiwiaWF0IjoxNDY4MjQxNzI3LCJleHAiOjE0NjgyODQ5MjcsImlzcyI6Imh0dHBzOi8vNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhLnByZWRpeC11YWEucnVuLmF3cy11c3cwMi1wci5pY2UucHJlZGl4LmlvL29hdXRoL3Rva2VuIiwiemlkIjoiNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhIiwiYXVkIjpbImFwcC1jbGllbnQtaWQiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZCIsInVhYSIsIm9wZW5pZCIsInByZWRpeC1hc3NldC56b25lcy4xMGEyMWM5NC0xYzQ0LTQ0ZGMtODZhMy05YTFjN2Q1MDU3NmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4Il19.vZvSnPKM3C-9npNcimTYeQRpzA9A8JARDxINX8rzPvxjNRXrQaASAEQkUY2YNhVez3aNoS4tQBFBPra8b1Z_3Wj26NDcCcvfuxrthAVaeGwKiPJVwIXP3QWyLzWrdWPzK7QMldpg5CdAva3HBfdGeiijMgesPoK4qX5lSJVCjOX1KIpX-_8GfLxjrHn1SPD_2DW04LY_vcKcsAI4ls2RRp4JVqKpeQYmA6zb1L1OB2KxPKWmHPoXNUEdNZx0u3QvMMe4fjqOADh-6i8mZHZca3moPewmulvvvC0z-w4hAa-Hcsh9a3jio0vHR4NDYJJ84uuGkFxtirei-yR2Dw2tVg',
+    'Authorization': 'bearer ' + uaaController.getToken(),
+    'Predix-Zone-Id': config.asset.zoneId,
+  'Content-Type' : 'application/json'
+  }
+};
+console.log("HERE: " + config.asset.ingestUri+'/'+ req.params.type + '/' + req.params.model);
+  // console.log(JSON.stringify(options, null, 2));
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("what is going on????");
+      self.model = JSON.parse(response.body);
+      //self.model  = JSON.stringify(self.modelA);
+      self.model[0].uri = self.model[0].uri + "*" + uuid1;
+      console.log("[INFO] Token fetched (expires in " +body + " seconds)." +self.model[0].uri +  "  !" + " dfs " +self.model[0].model); 
+    callback(200, self.model);
+    } else if (error || response.statusCode != 200) {
+      console.log("[INFO] Error fetching token: " + response.statusCode);
     }
   });
 };
 
-AssetController.prototype.sendPayload = function(body, callback) {
+// /////
+AssetController.prototype.processAsset = function(req, callback) {
+  console.log("[INFO] Fetching asset...(B TOKEN = ) ************ ");
   var self = this;
-  // Format payload with messageId as current time
-  var payload = {"messageId": this.date.getTime(), "body": body};
-
-  // If payload is not valid, respond with 422 error and error object and return
-  // else, continue
-  // if (PayloadValidator.validate(payload) === false){
-  //   console.log("[INFO] Invalid payload.");
-  //   callback(422, JSON.stringify({error: PayloadValidator.getErrors(payload)}));
-  //   return;
-  // }
+var nameLength = req.params.model.length;
+//var n = req.params.model.substring(0,nameLength-2);
+var n = req.params.model.substring(0,req.params.model.indexOf('-'));
 
 
-  var socket = new WebSocket(config.timeseries.ingestUri, { headers: this.headers });
-console.log("[INFO!!!!!!] WebSocket connection opened ?????. " + config.timeseries.ingestUri+ " : : " + this.headers.Authorization + " : : " );
-  socket.on('open', function open() {
-    console.log("[INFO] WebSocket connection opened.");
-    console.log("[INFO] sending payload with messageId: " + payload.messageId + "...");
-    console.log(JSON.stringify(payload, null, 2));
-    socket.send(JSON.stringify(payload), function ack(error) {
-      if (error) {
-        console.log("[INFO] Error sending payload " + payload.messageId);
-        callback(500, JSON.stringify({error: error + ' error here'}));
-      } else {
-        console.log("[INFO] payload " + payload.messageId + " successfully sent.");
-      }
+  var uuid1 = uuid.v1();
+  console.log("uuid = " + uuid1);    // -> '02a2ce90-1432-11e1-8558-0b488e4fc115')
+  var options = {
+  url: config.asset.ingestUri+'/'+ req.params.type + '/' + n,
+  //method: 'GET',
+  headers: {
+    //'Authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI3MGM0NjA3MS05MTk3LTQ2MDYtOTBmYi02YTJiNGVkM2QwNjEiLCJzdWIiOiJhcHAtY2xpZW50LWlkIiwic2NvcGUiOlsidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQuaW5nZXN0IiwidGltZXNlcmllcy56b25lcy5mYTZjODAwMC00Y2M4LTRmYTktYTMwNy0wNzVmYmZhYTg4OGQucXVlcnkiLCJ1YWEucmVzb3VyY2UiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZC51c2VyIiwib3BlbmlkIiwidWFhLm5vbmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuMTBhMjFjOTQtMWM0NC00NGRjLTg2YTMtOWExYzdkNTA1NzZlLnVzZXIiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4LnVzZXIiXSwiY2xpZW50X2lkIjoiYXBwLWNsaWVudC1pZCIsImNpZCI6ImFwcC1jbGllbnQtaWQiLCJhenAiOiJhcHAtY2xpZW50LWlkIiwiZ3JhbnRfdHlwZSI6ImNsaWVudF9jcmVkZW50aWFscyIsInJldl9zaWciOiIzYzA2Zjk2IiwiaWF0IjoxNDY4MjQxNzI3LCJleHAiOjE0NjgyODQ5MjcsImlzcyI6Imh0dHBzOi8vNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhLnByZWRpeC11YWEucnVuLmF3cy11c3cwMi1wci5pY2UucHJlZGl4LmlvL29hdXRoL3Rva2VuIiwiemlkIjoiNWYyZmI3ODEtZjE0Zi00ZjUwLTk4YWYtMDNlMzBmMzU1ZGNhIiwiYXVkIjpbImFwcC1jbGllbnQtaWQiLCJ0aW1lc2VyaWVzLnpvbmVzLmZhNmM4MDAwLTRjYzgtNGZhOS1hMzA3LTA3NWZiZmFhODg4ZCIsInVhYSIsIm9wZW5pZCIsInByZWRpeC1hc3NldC56b25lcy4xMGEyMWM5NC0xYzQ0LTQ0ZGMtODZhMy05YTFjN2Q1MDU3NmUiLCJwcmVkaXgtYXNzZXQuem9uZXMuNDVmN2U2NDUtOTFkNS00ZjRiLTg2YTAtOTgzZjlmNjdiYWU4Il19.vZvSnPKM3C-9npNcimTYeQRpzA9A8JARDxINX8rzPvxjNRXrQaASAEQkUY2YNhVez3aNoS4tQBFBPra8b1Z_3Wj26NDcCcvfuxrthAVaeGwKiPJVwIXP3QWyLzWrdWPzK7QMldpg5CdAva3HBfdGeiijMgesPoK4qX5lSJVCjOX1KIpX-_8GfLxjrHn1SPD_2DW04LY_vcKcsAI4ls2RRp4JVqKpeQYmA6zb1L1OB2KxPKWmHPoXNUEdNZx0u3QvMMe4fjqOADh-6i8mZHZca3moPewmulvvvC0z-w4hAa-Hcsh9a3jio0vHR4NDYJJ84uuGkFxtirei-yR2Dw2tVg',
+    'Authorization': 'bearer ' + uaaController.getToken(),
+    'Predix-Zone-Id': config.asset.zoneId,
+  'Content-Type' : 'application/json'
+  }
+};
+console.log("HERE: " + config.asset.ingestUri+'/'+ req.params.type + '/' + n);
+  // console.log(JSON.stringify(options, null, 2));
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("what is going on????");
+      self.model = JSON.parse(response.body);
+      //self.model  = JSON.stringify(self.modelA);
+      self.model[0].uri = "/test/"+self.model[0].uri.substr(self.model[0].uri.indexOf('G')) + "-uuid-" + uuid1; //tags goes here
+      self.model[0]["edge-alias"] = req.params.model;
+      self.model[0].sensors = "/sensors/"+n;
+      self.model[0].devices = req.body.devices;
+      self.model[0].kits =  req.body.kits;
+      console.log("[INFO] Token fetched (expires in " +body + " seconds)." +self.model[0].uri +  "  !" + " dfs " +self.model[0].devices + " :: "  + self.model[0].kits); 
+      //TRY TIMESERIES CALL
+
+      self.checkTS(self.model[0].uri,function(temp){
+      
+       //console.log("TS TS TS TS TEST " + temp + self.check);
+        if(temp == 0){
+          self.postGlobalAsset(self.model,function(temp){
+              console.log("[TRYING TO POST..........]");
+          });
+          callback(200, self.model);
+        }else{
+          callback(400, "NOT UNIQUE " + temp);
+        }
     });
+   
+    } else if (error || response.statusCode != 200) {
+      console.log("[INFO] Error fetching token: " + response.statusCode);
+    }
   });
+};
 
-  socket.on('message', function(data, flags) {
-    //callback(200, JSON.stringify(JSON.parse(data), null, 2));
-    callback(200, JSON.stringify({postStatus: "post was successful -- change!"}));
-    socket.close();
-    console.log("[INFO] WebSocket connection closed.");
-  });
+AssetController.prototype.checkTS = function(m, callback) {
+  console.log("[INFO] Fetching TS... ");
+  var self = this;
 
-  socket.on('error', function(error) {
-    // If 401 Unauthorized, refresh token and try again
-    // var errorCodeRegEx = /\d{3}/;
-    // if (error.toString().match(errorCodeRegEx)[0] == '401') {
-    //   console.log("[INFO] Token has expired - refreshing token and re-attempting send...");
-    //   uaaController.fetchToken(function(){
-    //     self.setHeaders();
-    //     self.sendPayload(body, function(status, message){
-    //       callback(status, message + ' eeeerrrooorrr');
-    //     });
-    //   });
-    // } else {
-      console.log("[INFO] " + error.toString());
-      callback(500, JSON.stringify({error: error + ' 500 here' }));
-   // }
-  });
+ // var uuid1 = uuid.v1();
+//console.log(' [INFO] Bearer ' + uaaControllerTS.getTokenTS() );    // -> '02a2ce90-1432-11e1-8558-0b488e4fc115')
+  var options = { method: 'POST',
+ url: 'https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/datapoints',
+ headers: 
+  { 
+    'cache-control': 'no-cache',
+    'predix-zone-id': 'ad85c18d-6ad0-463c-a4b2-175567ffa7ef',
+    'content-type': 'application/json',
+    authorization: 'Bearer ' + uaaControllerTS.getTokenTS() }, 
+ body: { start: '1y-ago', tags: [ { name: m } ] },
+ json: true };
+
+request(options, function (error, response, body) {
+ if (error) throw new Error(error + " hwew");
+
+ console.log(body);
+ // console.log(JSON.parse(body).tags);
+ // console.log(JSON.stringify(body).tags[0]);
+ //var jsonObj = JSON.parse(body);
+ console.log(body.tags[0].stats.rawCount);
+ this.check = body.tags[0].stats.rawCount;
+ callback(body.tags[0].stats.rawCount);
+});
+};
+
+
+AssetController.prototype.postGlobalAsset = function(req, callback) {
+  console.log("in piost " + JSON.stringify(req));
+  var options = { method: 'POST',
+  url: 'https://predix-asset.run.aws-usw02-pr.ice.predix.io/test',
+  headers: 
+   { 
+     'cache-control': 'no-cache',
+      authorization: 'Bearer ' + uaaController.getToken(),
+     'content-type': 'application/json;charset=utf-8',
+     'predix-zone-id': '10a21c94-1c44-44dc-86a3-9a1c7d50576e' },
+  body: JSON.stringify(req) };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+  callback(body);
+});
+};
+
+
+
+AssetController.prototype.getModel = function() {
+  return this.model;
+};
+AssetController.prototype.getTScheck = function() {
+  return this.check;
 };
 
 module.exports = new AssetController();
--- routes/index.js
+++ routes/index.js
@@ -6,76 +6,29 @@ var request = require('request');
 var config = require('../config/config.js');
 var UAAController = require('../controllers/UAAController');
 var uaaController = new UAAController();
-//var uaaTemp = '';
-//var TimeSeriesController = require('../controllers/TimeSeriesController');
-//var AssetController = require('../controllers/AssetController');
-var AssetControllerTest1 = require('../controllers/AssetControllerTest1');
+var AssetController = require('../controllers/AssetController');
 
 
-
-///new////
 
 
 // GET
 router.get('/:type/:model', function(req, res, next) {
 
-AssetControllerTest1.getAssets(req,function(status, message){
+AssetController.getAssets(req,function(status, message){
       res.status(status).send(message);
-      console.log("blah test :" + AssetControllerTest1.getModel()[0].uri +  " ; " + message[0].model);
+      console.log("blah test :" + AssetController.getModel()[0].uri +  " ; " + message[0].model);
 
     });
 
-
-
-
-
-
 });
 
-router.get('/TS/uuid/eli/:name', function(req, res, next) {
-console.log("TS HIT??");
-AssetControllerTest1.getTS(req,function(status, message){
-      res.send(JSON.stringify(message));
-      console.log("blah test " + message);
-
-    });
-
-
-
-
-
-
-});
-
-router.get('/kitAsset', function(req, res, next) {
-console.log("kitAsset HIT??");
-AssetControllerTest1.postKitAsset(req,function(status, message){
-      res.send(JSON.stringify(message));
-      console.log("blah test " + message);
-
-    });
-
-
-
-
-
-
-});
-
-//test
+//POST
 router.post('/demo/:type/:model', function(req, res, next) {
 
-AssetControllerTest1.getAssetsTEST(req,function(status, message){
+AssetController.processAsset(req,function(status, message){
       res.status(status).send(message);
-      console.log("blah test :" + AssetControllerTest1.getModel()[0].uri +  " ; " + message[0].model);
-      // AssetControllerTest1.getTSdEMO("aasdfasdf");
-      // console.log(AssetControllerTest1.getTScheck() + " ^&^&^^&&^^^&&^^")
+      console.log("blah test :" + AssetController.getModel()[0].uri +  " ; " + message[0].model);
     });
-
-
-
-
-
 
 });
 
